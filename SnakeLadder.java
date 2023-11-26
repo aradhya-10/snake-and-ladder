9065d7c6b464db57f22617d72f289091b08f97f6
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-11-26 10:05:57 +0530

    UC2: Roll the Dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Player {
 
     private int currentPosition;
@@ -13,11 +15,20 @@ class Player {
     public void setCurrentPosition(int currentPosition) {
         this.currentPosition = currentPosition;
     }
+	
+	public int rollDice() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
 }
 
 public class SnakeLadder {
 	public static void main(String[] args) {
 		Player P1 = new Player();
 		System.out.println("Player's initial position: " + P1.getCurrentPosition());
+		int move = P1.rollDice();
+		System.out.println("Dice roll: "+move);
+		P1.setCurrentPosition(P1.getCurrentPosition()+move);
+		System.out.println("Player's final position: " + P1.getCurrentPosition());
 	}
 }
